b7faeeca2620b2bd4e8aa19de85033fabd115dd5
Kenyon Ralph <quic_kralph@quicinc.com>
2024-12-06 14:16:35 -0800

    service: lint autofix

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,14 +7,14 @@
 # @example
 #   this is a private class - don't use directly
 class usbguard::service {
-  if $::usbguard::manage_service {
-    $enable_param = $::usbguard::service_ensure ? {
+  if $usbguard::manage_service {
+    $enable_param = $usbguard::service_ensure ? {
       'stopped' => false,
       default   => true,
     }
 
-    service { $::usbguard::service_name:
-      ensure => $::usbguard::service_ensure,
+    service { $usbguard::service_name:
+      ensure => $usbguard::service_ensure,
       enable => $enable_param,
     }
   }
